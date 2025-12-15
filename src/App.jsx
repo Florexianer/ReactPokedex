99eb1e8905c21c6eba0d54dbb134023e16649f43
Florexianer <8111@htl.rennweg.at>
2025-12-15 23:21:34 +0100

    initial app

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function App() {
+  const name = 'test';
+  
+  return (
+    <>
+      <div className="text-2xl">
+        App
+      </div>
+    </>
+  )
+}
+
+export default App
